15c91aa71de6de5f33151ceaadc281186bb62094
daghb <dagh@baardsen.org>
2017-02-07 14:03:46 +0100

    Cleanup to default logtrail

--- addjson.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-md build\kibana\logtrail\
-copy logtrail.json build\kibana\logtrail\
-cd build
-"c:\Program Files\7-zip\7z.exe" a logtrail-0.1.7.zip kibana\logtrail\logtrail.json
-cd ..
--- rebuild.bat
+++
@@ -1,5 +0,0 @@
-call npm run build
-call addjson.cmd
-call ..\kibana-5.1.2\bin\kibana-plugin.bat remove logtrail
-call ..\kibana-5.1.2\bin\kibana-plugin.bat install file://d:/dev/kibana/logtrail/build/logtrail-0.1.7.zip
-call ..\kibana-5.1.2\bin\kibana.bat
