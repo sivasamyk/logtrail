4580f856760274a0533bafe2f7738989e5532edf
skaliappan <sivasamyk@gmail.com>
2017-09-12 19:56:42 +0530

    fix for issue #174. Make sure the acutal log message is escaped below applying handlebar template. incase of highlight use non-html pre & post tag and replace them later. tested with highlight and non-highlight

--- public/app.js
+++ public/app.js
@@ -533,20 +533,6 @@ uiModules.get('app/logtrail').directive('onLastRepeat', function () {
   };
 });
 
-uiModules.get('app/logtrail').directive('compileTemplate', function($compile, $parse) {
-  return {
-    link: function(scope, element, attr){
-      var parsed = $parse(attr.ngBindHtml);
-      function getStringValue() { return (parsed(scope) || '').toString(); }
-
-      //Recompile if the template changes
-      scope.$watch(getStringValue, function() {
-        $compile(element, null, -9999)(scope);  //The -9999 makes it skip directives so that we do not recompile ourselves
-      });
-    }
-  }
-})
-
 uiModules.get('app/logtrail').directive('clickOutside', function ($document) {
   return {
     restrict: 'A',
