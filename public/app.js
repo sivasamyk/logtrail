2bff1d3aaed302ba0763ea3a51632d285933cbb3
skaliappan <sivasamyk@gmail.com>
2017-09-13 23:03:18 +0530

    introduced compileTemplate back

--- public/app.js
+++ public/app.js
@@ -561,3 +561,18 @@ uiModules.get('app/logtrail').directive('clickOutside', function ($document) {
     }
   };
 });
+
+//This is required for onClick event in custom message formats
+uiModules.get('app/logtrail').directive('compileTemplate', function($compile, $parse) {
+  return {
+    link: function(scope, element, attr){
+      var parsed = $parse(attr.ngBindHtml);
+      function getStringValue() { return (parsed(scope) || '').toString(); }
+
+      //Recompile if the template changes
+      scope.$watch(getStringValue, function() {
+        $compile(element, null, -9999)(scope);  //The -9999 makes it skip directives so that we do not recompile ourselves
+      });
+    }
+  }
+});
