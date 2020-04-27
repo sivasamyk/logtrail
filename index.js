e8a08d5c9971d6a50eea922a10a0f3daa9d99ebd
Sivasamy Kaliappan <sivasamyk@gmail.com>
2020-04-27 11:43:58 +0530

    support for 7.6.2

--- index.js
+++ index.js
@@ -13,7 +13,37 @@ export default function (kibana) {
       hacks: [
       ]
     },
-    init: function (server, options) {
+    init: function (server) {
+      // register feature
+      console.log(JSON.stringify(server.plugins));
+      const xpackMainPlugin = server.plugins.xpack_main;
+      console.log(`****** xpackMainPlugin - ${xpackMainPlugin}`);
+      if (xpackMainPlugin) {
+        xpackMainPlugin.registerFeature({
+          id: 'logtrail',
+          name: 'LogTrail',
+          app: ['logtrail','kibana'],
+          catalogue: [],
+          privileges: {
+            all: {
+              api: [],
+              savedObject: {
+                all: [],
+                read: [],
+              },
+              ui: ['show'],
+            },
+            read: {
+              api: [],
+              savedObject: {
+                all: [],
+                read: [],
+              },
+              ui: ['show'],
+            },
+          },
+        });
+      }
       // Add server routes and initalize the plugin here
       serverRoute(server);
     }
